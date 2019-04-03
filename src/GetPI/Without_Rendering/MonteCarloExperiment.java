aef399ce347083b5db58b2280c93400b387e3437
Daolinzhou <466118115@qq.com>
2019-04-02 23:06:30 -0700

    Probability problem

--- src/GetPI/Without_Rendering/MonteCarloExperiment.java
+++ src/GetPI/Without_Rendering/MonteCarloExperiment.java
@@ -1,8 +1,53 @@
 package GetPI.Without_Rendering;
 
+import GetPI.Circle;
+import GetPI.MonteCarloPiData;
+
+import java.awt.*;
+
 /**
  * Create by Daolin
  * on 04 / 2019
  */
 public class MonteCarloExperiment {
+
+    private int squareSide;
+    private int N;
+    private int outputInterval = 100;
+
+    public MonteCarloExperiment(int squareSide, int N) {
+        if(squareSide <= 0 || N <= 0)
+            throw new IllegalArgumentException("squareSide and N must larger than 0");
+        this.squareSide = squareSide;
+        this.N = N;
+    }
+
+    public void setOutputInterval(int interval){
+        if(interval <= 0)
+            throw new IllegalArgumentException("interval should larger than 0");
+        this.outputInterval = interval;
+    }
+
+    public void run(){
+        Circle circle = new Circle(squareSide/2, squareSide/2, squareSide/2);
+        MonteCarloPiData data = new MonteCarloPiData(circle);
+        for(int i = 0; i < N; i++){
+            if(i % outputInterval == 0)
+                System.out.println(data.estimatePI());
+            int x = (int) (Math.random() * squareSide);
+            int y = (int) (Math.random() * squareSide);
+            data.addPoint(new Point(x, y));
+        }
+    }
+
+    public static void main(String[] args) {
+        int squareSide = 800;
+        int N = 1000_0000;
+
+        MonteCarloExperiment esp = new MonteCarloExperiment(squareSide, N);
+        esp.setOutputInterval(10000);
+        esp.run();
+
+    }
+
 }
--- src/ThreeGateExperiment/ThreeGateExperiment.java
+++ src/ThreeGateExperiment/ThreeGateExperiment.java
@@ -5,4 +5,41 @@ package ThreeGateExperiment;
  * on 04 / 2019
  */
 public class ThreeGateExperiment {
+
+    private int N;
+
+    public ThreeGateExperiment(int n) {
+        if(n <= 0)
+            throw new IllegalArgumentException("N must larger than 0");
+        N = n;
+    }
+
+    public void run(boolean changeDoor){
+        int wins = 0;
+        for(int i = 0; i < N; i++)
+            if(play(changeDoor))
+                wins++;
+        System.out.println(changeDoor ? "Change Door" : "Not Change Door");
+        System.out.println("winning rate:" + (double) wins/N);
+    }
+
+    private boolean play(boolean changeDoor){
+        // Door 0, 1, 2
+        int prizeDoor = (int) (Math.random() * 3);
+        int playerChoice = (int) (Math.random() * 3);
+
+        if (playerChoice == prizeDoor)
+            return ! changeDoor;
+        else
+            return changeDoor;
+    }
+
+    public static void main(String[] args) {
+        int N = 1000_0000;
+        ThreeGateExperiment exp = new ThreeGateExperiment(N);
+        exp.run(true);
+        System.out.println();
+        exp.run(false);
+    }
+
 }
